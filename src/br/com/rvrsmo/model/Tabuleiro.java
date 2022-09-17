6bbe0b54c36201793baf307725a32c38ba3cf440
rbverissimo <rverissimo.dev@gmail.com>
2022-09-17 17:09:08 -0300

    creating Tabuleiro class logic

--- src/br/com/rvrsmo/model/Tabuleiro.java
+++ src/br/com/rvrsmo/model/Tabuleiro.java
@@ -1,5 +1,14 @@
 package br.com.rvrsmo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+	
+	private int QtdeLinhas;
+	private int QtdeColunas;
+	private int QtdeMinas;
+	
+	private final List<Campo> campos = new ArrayList<>();
 
 }
