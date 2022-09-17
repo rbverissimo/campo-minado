2661b1efc3e6b9598f223ec885bb2b7814c042c5
rbverissimo <rverissimo.dev@gmail.com>
2022-09-17 17:37:58 -0300

    updating Tabuleiro class

--- src/br/com/rvrsmo/model/Tabuleiro.java
+++ src/br/com/rvrsmo/model/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.rvrsmo.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 	
@@ -11,4 +12,61 @@ public class Tabuleiro {
 	
 	private final List<Campo> campos = new ArrayList<>();
 
+	public Tabuleiro(int qtdeLinhas, int qtdeColunas, int qtdeMinas) {
+		QtdeLinhas = qtdeLinhas;
+		QtdeColunas = qtdeColunas;
+		QtdeMinas = qtdeMinas;
+		
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+		
+	}
+
+
+	private void gerarCampos() {
+		
+		for (int i = 0; i < QtdeLinhas; i++) {
+			for(int j = 0; i < QtdeColunas; j++) {
+				campos.add(new Campo(i, j));
+			}
+			
+		}
+		
+	}
+	
+	
+	private void associarVizinhos() {
+		
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2); 		
+			}	
+		}
+		
+	}
+	
+	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		do {
+			
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).setMinado();
+			
+		} while (minasArmadas < QtdeMinas); 
+
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.isLimpo());
+	}
+
+	public void reiniciarTabuleiro() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
 }
