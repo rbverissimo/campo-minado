9a259607f9f8613cb0a4ee122e396d564e2c69fd
rbverissimo <rverissimo.dev@gmail.com>
2022-09-23 15:34:01 -0300

    Building the view Console logic

---
+++ src/br/com/rvrsmo/view/TabuleiroTerminal.java
@@ -0,0 +1,5 @@
+package br.com.rvrsmo.view;
+
+public class TabuleiroTerminal {
+
+}
---
+++ test/br/com/rvrsmo/view/TabuleiroTerminalTeste.java
@@ -0,0 +1,14 @@
+package br.com.rvrsmo.view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TabuleiroTerminalTeste {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
